4af6c7e10c42070b5db0ca524485894ed84a8846
reynah762 <reynah762@gmail.com>
2025-05-22 10:36:04 +0300

    notifictaion history display

--- config/mysql_connection.js
+++ config/mysql_connection.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "wcf_db",
-  process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
+  process.env.DB_NAME || "asterisk",
+  process.env.DB_USER || "asterisk",
+  process.env.DB_PASS || "@Ttcl123",
   {
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
