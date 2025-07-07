2e5641cc1ab0d73213dbe8aecab0a5378483c65a
Kamasho <kamashogodfrey997@gmail.com>
2025-07-07 11:02:50 +0300

    for find error

--- config/mysql_connection.js
+++ config/mysql_connection.js
@@ -1,37 +1,13 @@
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config();
-
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME || "asterisk",
-//   process.env.DB_USER || "asterisk",
-//   process.env.DB_PASS || "@Ttcl123",
-//   {
-//     host: process.env.DB_HOST || "10.52.0.19",
-//     port: process.env.DB_PORT || 3306,
-//     dialect: "mysql",
-//     logging: false,
-//     dialectOptions: { connectTimeout: 10000 },
-//   }
-// );
-
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("Connected to the live MySQL database..."))
-//   .catch((err) => console.error("MySQL Connection Error:", err));
-
-// module.exports = sequelize;
-
-
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
+
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || "asterisk",
   process.env.DB_USER || "asterisk",
-  process.env.DB_PASS || "Wcf@1234",
+  process.env.DB_PASS || "@Ttcl123",
   {
-    host: process.env.DB_HOST || "192.168.1.170",
+    host: process.env.DB_HOST || "10.52.0.19",
     port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: false,
@@ -45,4 +21,28 @@ sequelize
   .catch((err) => console.error("MySQL Connection Error:", err));
 
 module.exports = sequelize;
+
+
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || "asterisk",
+//   process.env.DB_USER || "asterisk",
+//   process.env.DB_PASS || "Wcf@1234",
+//   {
+//     host: process.env.DB_HOST || "192.168.1.170",
+//     port: process.env.DB_PORT || 3306,
+//     dialect: "mysql",
+//     logging: false,
+//     dialectOptions: { connectTimeout: 10000 },
+//   }
+// );
+
+// sequelize
+//   .authenticate()
+//   .then(() => console.log("Connected to the live MySQL database..."))
+//   .catch((err) => console.error("MySQL Connection Error:", err));
+
+// module.exports = sequelize;
 
