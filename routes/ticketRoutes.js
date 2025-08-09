a6c884ecd4f3e172540d6912ea0c8de8490f5c00
reynah762 <reynah762@gmail.com>
2025-08-09 12:55:07 +0300

    ticket route fix

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -17,6 +17,7 @@ const { roleMiddleware } = require("../middleware/roleMiddleware");
 const { body } = require('express-validator'); // For validation
 const router = express.Router();
 const { Op } = require("sequelize");
+const { uploadSingle } = require('../config/multerConfig');
 
 // Import enhanced multer configuration
 const { 
