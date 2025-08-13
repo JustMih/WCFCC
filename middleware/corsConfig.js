48210f0441150717e8b1b5244ffbdb57ad05fa5b
reynah762 <reynah762@gmail.com>
2025-08-13 11:56:41 +0300

    cors allowing ip

--- middleware/corsConfig.js
+++ middleware/corsConfig.js
@@ -9,11 +9,10 @@ const externalCorsOptions = {
     // List of allowed origins
     const allowedOrigins = process.env.ALLOWED_ORIGINS ? 
       process.env.ALLOWED_ORIGINS.split(',') : 
-      ['http://localhost:5070', 'http://127.0.0.1:8005', 
+      ['http://localhost:3000', 'http://127.0.0.1:3000', 
+        'http://localhost:5070', 'http://127.0.0.1:8005', 
         'http://192.168.21.70:5070',
         'https://192.168.21.70',
-        'http://192.168.21.70:5070/api',
-        'https://192.168.21.70/api',
         'http://192.168.21.70:5075',
       ];
     
