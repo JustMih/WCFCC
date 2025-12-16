e0185306fb6b00ca0e329eb40844ba28313750c4
reynah762 <reynah762@gmail.com>
2025-12-16 14:50:12 +0300

    changes on update creation for button to change color and show counts

--- config/config.js
+++ config/config.js
@@ -2,17 +2,6 @@
 require("dotenv").config();
 
 module.exports = {
-<<<<<<< HEAD
-  // development: {
-  //   username: process.env.DB_USER || "asterisk",
-  //   password: process.env.DB_PASS || "@Ttcl123",
-  //   database: process.env.DB_NAME || "asterisk",
-  //   host: process.env.DB_HOST || "10.52.0.19",
-  //   port: process.env.DB_PORT || 3306,
-  //   dialect: "mysql",
-  //   logging: false,
-  // },
-=======
   development: {
     username: process.env.DB_USER || "asterisk",
     password: process.env.DB_PASS || "Wcf@1234",
@@ -22,7 +11,6 @@ module.exports = {
     dialect: "mysql",
     logging: false,
   },
->>>>>>> 3f323cc61538eecc27eb14408112a8e9f560dee7
   production: {
     username: process.env.DB_USER || "asterisk",
     password: process.env.DB_PASS || "Wcf@1234",
