b24bdbda7fc6fba48098729015420110374246dd
Makeja <paul.makeja@ttcl.co.tz>
2026-02-04 09:00:43 +0300

    before merge solve conflict

--- models/missedcall.js
+++ models/missedcall.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/mysql_connection.js");
 
@@ -38,38 +37,4 @@ MissedCall.associate = function (models) {
   });
 };
 
-=======
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/mysql_connection.js");
-
-const MissedCall = sequelize.define("MissedCall", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  caller: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  time: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  agentId: {
-    type: DataTypes.STRING, // or UUID if you use that format
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.ENUM('pending', 'called_back', 'ignored'),
-    defaultValue: 'pending',
-    allowNull: false,
-  },
-}, {
-  timestamps: true, // adds createdAt and updatedAt
-});
-
-
-
->>>>>>> d60bce46dafbb4d57873619231b42e891f54935c
 module.exports = MissedCall;
--- routes/livestreamRoutes.js
+++ routes/livestreamRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  const express = require("express");
 const router = express.Router();
 const { spyOnCall } = require("../controllers/spyController");
@@ -12,17 +11,3 @@ router.get("/live-calls", getAllLiveCalls);
 router.post("/spy", spyOnCall);
 
 module.exports = router;
-=======
- const express = require("express");
-const router = express.Router();
-//get all live calls
-const {
-  getAllLiveCalls,
-  
-} = require("../controllers/livestream/livestreamController");
-
-router.get("/live-calls", getAllLiveCalls);
- 
-
-module.exports = router;
->>>>>>> d60bce46dafbb4d57873619231b42e891f54935c
